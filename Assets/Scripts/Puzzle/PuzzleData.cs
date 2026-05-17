0b53479bed80f929b24ba42e7536c216609424ee
Evan-Rangel <evan.rangel@uniat.edu.mx>
2026-05-17 16:33:29 -0600

    Editor cambios

--- Assets/Scripts/Puzzle/PuzzleData.cs
+++ Assets/Scripts/Puzzle/PuzzleData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPuzzleData", menuName = "Events/PuzzleData")]
 public class PuzzleData : ScriptableObject
 {
-    [field: SerializeField, SerializeReference, SubclassSelector] public PuzzleBehaviour puzzleBehaviour { get; private set; }
+    [SerializeReference, SubclassSelector]public PuzzleBehaviour adas;
     public string puzzleName;
     public string description;
 }
